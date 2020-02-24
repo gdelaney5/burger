81fa9d036d4ed22fa39140f51a7c7dce4c4b4dc8
Garrett Delaney <gdelaney@wep-inc.com>
2020-02-23 18:52:21 -0800

    created necessary files, directories and routes

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server Listening on http://localhost:" + PORT);
+});
+
